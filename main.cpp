e97452eda8078bdf2100769c956db8812ce19d3a
w <exclide1@gmail.com>
2023-11-17 21:40:29 +0300

    refact

--- main.cpp
+++ main.cpp
@@ -1,15 +1,16 @@
 #include "ChatServer.h"
 
 int main(int argc, char* argv[]) {
-    if (argc != 2) {
+    if (argc > 2) {
         std::cerr << "Correct usage: serv <port>\n";
         return EXIT_FAILURE;
     }
 
     const int numThreads = 3;
+    int port = argc == 2 ? std::stoi(argv[1]) : 1234;
 
     io_context ioc{numThreads}; //hint number of threads running ioc
-    tcp::endpoint endpoint(tcp::v4(), std::stoi(argv[1]));
+    tcp::endpoint endpoint(tcp::v4(), port);
 
     std::make_shared<ChatServer>(ioc, endpoint)->StartAccept();
     //keep the temp shared alive using shared from this
